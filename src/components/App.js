5d61e0ce47d16f50f83a780dce8e82c2ad50d355
naveen-kk4 <naveennig2001@gmail.com>
2023-12-23 11:58:24 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,42 @@
 
-import React from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+   const [todo,setTodo] = useState("");
+   const[list,setList] = useState({});
+   const[idx,setIdx] = useState(0);
+
+   function handleSubmit(e){
+    e.preventDefault();
+        setList({...list,[idx]:todo});
+        setIdx(idx+1);
+        setTodo("");
+        
+    }
+    function handleDelete(e){
+     
+      delete list[e.target.id]; 
+      setList({...list});
+    }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>To-Do List</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" required onChange={(e)=>setTodo(e.target.value)} value={todo}></input>
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {
+              Object.keys(list).map((keyName) => (
+                <li>
+                    <span>{list[keyName]}</span>
+                    <button id={keyName} type="delete" onClick={handleDelete}>Delete</button>
+                </li>
+            ))
+        }
+      </ul>
+       
     </div>
   )
 }
